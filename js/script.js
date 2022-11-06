07d6c3e5289074427cf6daa51cf6101c5cd00f79
Angelika Stano <archiangel809@gmail.com>
2022-11-06 12:46:34 +0100

    check mark to task.done added

--- js/script.js
+++ js/script.js
@@ -42,12 +42,11 @@
 
         for (const task of tasks) {
             htmlString += `
-        <li
-        class="list__item${task.done ? " list__item--done" : ""}"
-        >
-          <button class="list__button--done js-done"></button>
-                    ${task.content}
-                    <button class="list__button--remove js-remove">🗑</button>
+            
+        <li class="list__item : ""}">
+          <button class="list__button list__button--done js-done">${task.done ? "✓" : ""}</button>
+            <p class=" ${task.done ? "list__item--done" : ""}">${task.content}</p>
+          <button class="list__button list__button--remove js-remove">🗑</button>
         </li>
         
         `;

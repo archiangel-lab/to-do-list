1b011696cd06145acb9bbc7e119207d8aee6e071
Angelika Stano <archiangel809@gmail.com>
2022-11-20 12:11:21 +0100

    copy MOD06 added

---
+++ js/MOD06.js
@@ -0,0 +1,91 @@
+{
+    const welcome = () => {
+        console.log("Witaj! Zapraszam do sprawdzenie mojej pracy. Dziękuję za wszelkie uwagi i wskazówki :)");
+    }
+
+    const tasks = [];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButtons, index) => {
+            removeButtons.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+              <li class="list__item">
+                <button class="list__button list__button--done js-done">
+                  ${task.done ? "✓" : ""}
+                </button>
+                <p class=" ${task.done ? "list__item--done" : ""}">${task.content}</p>
+                <button class="list__button list__button--remove js-remove">
+                 🗑
+                </button>
+              </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        bindEvents();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskObject = document.querySelector(".js-newTask");
+        const newTaskContent = newTaskObject.value.trim();
+
+        if (newTaskContent !== "") {
+            addNewTask(newTaskContent);
+            newTaskObject.value = "";
+        }
+
+        newTaskObject.focus();
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+
+        welcome();
+    };
+
+    init();
+}
